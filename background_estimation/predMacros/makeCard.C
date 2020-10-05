0a64f7163e2eba2291b5f63b51ead62e2d9744a4
Brent Stone <brentstone01@gmail.com>
2020-10-05 16:16:15 -0700

    adding JMS, JMR, tau21 things, cleaning up organization of code

--- background_estimation/predMacros/makeCard.C
+++ background_estimation/predMacros/makeCard.C
@@ -1,6 +1,8 @@
 #if !defined(__CINT__) || defined(__MAKECINT__)
 #include "../predTools/DataCardMaker.h"
+#include "../predTools/SystConstants.h"
 using namespace CutConstants;
+using namespace SystConstants;
 using namespace ASTypes;
 
 
@@ -22,29 +24,31 @@ std::string getSystName (const std::string& prefix, const std::string& proc,cons
     return sn;
 };
 
+//Make search variables
+void makeSearchVars(DataCardMaker& card) {
+    card.addVar(MOD_MJ,100,0,1000,false);
+    card.addVar(MOD_MR,1000,0,10000,false);
+    card.addVar(MOD_MS,2000,true);
+};
+
+void addSDMassSysts(DataCardMaker& card, std::string yr) {
+    card.addParamSystematic("hbb_scale"+yr,    sf_sdmass_scale_Run2-1.0,unc_sdmass_scale_Run2);
+    card.addParamSystematic("hbb_scale_top"+yr,sf_sdmass_scale_Run2-1.0,unc_sdmass_scale_Run2);
+    card.addParamSystematic("hbb_res"+yr,    sf_sdmass_res_Run2-1.0,unc_sdmass_res_Run2);
+    card.addParamSystematic("hbb_res_top"+yr,sf_sdmass_res_Run2-1.0,unc_sdmass_res_Run2);
+};
+
 void go(const int insig, const std::string& filename, const std::string& mainDir, REGION reg, int channel){
     std::string sigInputDir =  mainDir +  "/signalInputs/";
     std::string sfPF = sigInputDir + filename;
     std::string signalName = signals[insig];
-
     std::string fPF;
-//    switch(reg){
-//    case REG_SR:
-//        fPF = mainDir + "/bkgInputs/" + filename;
-//        break;
-//    case REG_TOPCR:
-//        fPF = mainDir + "/bkgInputsTopCR/" + filename + "_TopCR";
-//        break;
-//    case REG_NONTOPCR:
-//        fPF = mainDir + "/bkgInputsNonTopCR/" + filename + "_NonTopCR";
-//        break;
-//    }
 
     std::string cmd = "combineCards.py ";
 
     bool doCombinedRun2 = true;
-    std::vector<std::string> years = {"2016","2017","2018"};
-    if(doCombinedRun2) years = {"Run2"};
+    std::vector<std::string> years = {"Run2"};
+    if(!doCombinedRun2) years = {"2016","2017","2018"};
 
     for(std::string& yr : years) {
     	sigInputDir = mainDir + "/signalInputs/";
@@ -62,52 +66,52 @@ void go(const int insig, const std::string& filename, const std::string& mainDir
         }
 
         yr = std::string("_")+yr;
-    if(channel == 0 || channel == 1) {
-        for(const auto& l :lepCats) for(const auto& b :btagCats) for(const auto& p :purCats) for(const auto& h :selCuts1){
-            if(l == lepCats[LEP_EMU] ) continue;
-            if(b == btagCats[BTAG_LMT]) continue;
-            if(p == purCats[PURE_I] ) continue;
-            if(h != selCuts1[SEL1_FULL] ) continue;
 
-            const std::string cat = l +"_"+b+"_"+p +"_"+h;
+        if(channel == 0 || channel == 1) {
+            for(const auto& l :lepCats) for(const auto& b :btagCats) for(const auto& p :purCats) for(const auto& h :selCuts1){
+                if(l == lepCats[LEP_EMU] ) continue;
+                if(b == btagCats[BTAG_LMT]) continue;
+                if(p == purCats[PURE_I] ) continue;
+                if(h != selCuts1[SEL1_FULL] ) continue;
 
+                const std::string cat = l +"_"+b+"_"+p +"_"+h;
 
-            auto card = DataCardMaker(cat,"13TeV"+yr,1);
-            const std::string dataCardTag = DataCardMaker::getFileNameTag(cat,"13TeV"+yr);
-            cmd += std::string(" ")+ dataCardTag+"="+DataCardMaker::getOutputCardFileName(dataCardTag);
+                auto card = DataCardMaker(cat,"13TeV"+yr,1);
+                const std::string dataCardTag = DataCardMaker::getFileNameTag(cat,"13TeV"+yr);
+                cmd += std::string(" ")+ dataCardTag+"="+DataCardMaker::getOutputCardFileName(dataCardTag);
 
-            auto fullInputName =[&](const std::string& proc, const std::string& l, const std::string& b, const std::string& p, const std::string& h, const std::string& pf) -> std::string
+                auto fullInputName =[&](const std::string& proc, const std::string& l, const std::string& b, const std::string& p, const std::string& h, const std::string& pf) -> std::string
                     {return fPF + "_"+proc +"_"+l +"_"+b+"_"+p +"_"+h +"_"+ pf; };
-            auto inputName =[&](const std::string& proc, const std::string& pf) -> std::string {return fPF + "_"+proc +"_"+cat +"_"+ pf; };
-            auto signalInputName =[&](const std::string& proc, const std::string& pf) -> std::string {return sfPF + "_"+proc +"_"+cat +"_"+ pf; };
+                auto inputName =[&](const std::string& proc, const std::string& pf) -> std::string {return fPF + "_"+proc +"_"+cat +"_"+ pf; };
+                auto signalInputName =[&](const std::string& proc, const std::string& pf) -> std::string {return sfPF + "_"+proc +"_"+cat +"_"+ pf; };
 
-            auto systName = [&](const std::string& proc,const std::string& name, const std::string& sel = "-1")->std::string {
-                return getSystName("",proc,name, sel == "-1" ? l +"_"+b+"_"+p : sel  );
-            };
+                auto systName = [&](const std::string& proc,const std::string& name, const std::string& sel = "-1")->std::string {
+                    return getSystName("",proc,name, sel == "-1" ? l +"_"+b+"_"+p : sel  );
+                };
+
+                makeSearchVars(card);
 
 //            auto addSignalSystematics = [&]() {
 //            	card.addSystematic("yield","lnN",{{signalName,systematics.yield}});
 //            };
 
-            //Make search variables
-            card.addVar(MOD_MJ,100,0,1000,false);
-            card.addVar(MOD_MR,1000,0,10000,false);
-            card.addVar(MOD_MS,2000,true);
 
-            //---------------------------------------------------------------------------------------------------
-            //Get rates and contributions for backgrounds
-            //---------------------------------------------------------------------------------------------------
-            card.addFixedYieldFromFile(bkgSels[BKG_QG],1,fPF+"_"+bkgSels[BKG_QG]+"_distributions.root",bkgSels[BKG_QG]+"_"+cat+"_"+hhMCS,1.0,true);
-            double rate_lostTW = card.addFixedYieldFromFile(bkgSels[BKG_LOSTTW],2,fPF+"_"+bkgSels[BKG_LOSTTW]+"_distributions.root",bkgSels[BKG_LOSTTW]+"_"+cat+"_"+hhMCS,1.0,true);
-            double rate_mw =     card.addFixedYieldFromFile(bkgSels[BKG_MW],3,fPF+"_"+bkgSels[BKG_MW]+"_distributions.root",bkgSels[BKG_MW]+"_"+cat+"_"+hhMCS,1.0,true);
-            double rate_mt =     card.addFixedYieldFromFile(bkgSels[BKG_MT],4,fPF+"_"+bkgSels[BKG_MT]+"_distributions.root",bkgSels[BKG_MT]+"_"+cat+"_"+hhMCS,1.0,true);
 
-            //---------------------------------------------------------------------------------------------------
-            //Add Systematics first since the param systs need to have the variables added to the workspace
-            //---------------------------------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------------
+    //Get rates and contributions for backgrounds
+    //---------------------------------------------------------------------------------------------------
+                card.addFixedYieldFromFile(bkgSels[BKG_QG],1,fPF+"_"+bkgSels[BKG_QG]+"_distributions.root",bkgSels[BKG_QG]+"_"+cat+"_"+hhMCS,1.0,true);
+                double rate_lostTW = card.addFixedYieldFromFile(bkgSels[BKG_LOSTTW],2,fPF+"_"+bkgSels[BKG_LOSTTW]+"_distributions.root",bkgSels[BKG_LOSTTW]+"_"+cat+"_"+hhMCS,1.0,true);
+                double rate_mw =     card.addFixedYieldFromFile(bkgSels[BKG_MW],3,fPF+"_"+bkgSels[BKG_MW]+"_distributions.root",bkgSels[BKG_MW]+"_"+cat+"_"+hhMCS,1.0,true);
+                double rate_mt =     card.addFixedYieldFromFile(bkgSels[BKG_MT],4,fPF+"_"+bkgSels[BKG_MT]+"_distributions.root",bkgSels[BKG_MT]+"_"+cat+"_"+hhMCS,1.0,true);
+
+    //---------------------------------------------------------------------------------------------------
+    //Add Systematics first since the param systs need to have the variables added to the workspace
+    //---------------------------------------------------------------------------------------------------
+
             //luminosity
 //            card.addSystematic("yield","lnN",{{signalName,1.0391}});//lumi = 2.5 pdf= 2, PU = 0.5, btagfake=1
-            card.addSystematic("yield"+yr,"lnN",{{signalName,doCombinedRun2?1.12:1.2}}); //testing
+              card.addSystematic("yield"+yr,"lnN",{{signalName,doCombinedRun2?1.12:1.2}}); //testing
 
 
             //lepton efficiency
@@ -118,10 +122,11 @@ void go(const int insig, const std::string& filename, const std::string& mainDir
 
             //tau21
 //            card.addParamSystematic("tau21_PtDependence",0.0,0.041);
-//            if(p == purCats[PURE_HP])
-//                card.addSystematic(systName("","tau21_eff",""),"lnN",{{signalName,1+0.14}});
-//            if(p == purCats[PURE_LP])
-//                card.addSystematic(systName("","tau21_eff",""),"lnN",{{signalName,1-0.33}});
+            if(p == purCats[PURE_LP])
+                card.addSystematic(systName("","tau21_eff",""),"lnN",{{signalName,1-unc_tau21_LP_Run2}});
+
+            if(p == purCats[PURE_HP])
+                card.addSystematic(systName("","tau21_eff",""),"lnN",{{signalName,1+unc_tau21_HP_Run2}});
 
 
             //Btag
@@ -135,24 +140,26 @@ void go(const int insig, const std::string& filename, const std::string& mainDir
 //            card.addParamSystematic("jes"+yr,0.0,0.01);
 //            card.addParamSystematic("jer"+yr,0.0,0.01);
 
-            card.addParamSystematic("hbb_scale"+yr,0.0,0.0094);
-            card.addParamSystematic("hbb_res"+yr,0.0,0.2);
-            //KDE shape systematics
+            // soft-drop mass scale and resolution (apply to mbb)
+            addSDMassSysts(card,yr);
+
+            // QG bkg KDE shape systematics (X = mbb, Y = mhh -- morphing: PT = m, OPT = 1/m)
             card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"PTX",b) ,0.0,0.5);
             card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"OPTX",b),0.0,1.0);
             card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"PTY")   ,0.0,1.0);
             card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"OPTY")  ,0.0,1.0);
-    //        card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"PT2Y") ,0.0,1.0);
 
-            //top HH resolution and scale
+            // lost tW mbb resolution and scale
+            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"PTX" ,b),0.0,0.3);
+            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"OPTX",b),0.0,0.6);
+
+            //top HH resolution and scale (correlate all top-derived bkgs: lost tW, mW, mT)
             card.addParamSystematic(systName("top","res"  ) ,0.0,0.20);
             card.addParamSystematic(systName("top","scale") ,0.0,0.25);
             card.addParamSystematic(systName("top","mt_rel_scale",b) ,0.0,0.25);
             card.addParamSystematic(systName("top","lostmw_rel_scale",b) ,0.0,0.25);
-            //top Hbb resolution and scale
-            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"PTX" ,b),0.0,0.3);
-            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"OPTX",b),0.0,0.6);
-            //Normalization
+
+            // Normalization (correlate top-derived bkgs)
             card.addSystematic(systName(bkgSels[BKG_QG],"norm")  ,"lnN",{{bkgSels[BKG_QG],1.5}});
             card.addSystematic(systName("top","norm")            ,"lnN",{{bkgSels[BKG_LOSTTW],1.25},{bkgSels[BKG_MW],1.25},{bkgSels[BKG_MT],1.25}});
 
@@ -171,19 +178,20 @@ printf("dbg0\n");
 //                                 {{"hbb_scale"+yr,1}},{{"hbb_res"+yr,1}},{{"unclust"+yr,0.5},{"jes"+yr,1},{"jer"+yr,0.5}},{{"unclust"+yr,0.5},{"jes"+yr,2},{"jer"+yr,5}}, b == btagCats[BTAG_L],MOD_MS);
 
 			card.add2DSignalParametricShape(signalName,MOD_MJ,MOD_MR, signalInputName(signalName,"2D_fit.json"),
-                     StrFlts(),StrFlts(),StrFlts(),StrFlts(), b == btagCats[BTAG_L],MOD_MS);
+					{{"hbb_scale"+yr,1}},{{"hbb_res"+yr,1}},StrFlts(),StrFlts(), b == btagCats[BTAG_L],MOD_MS);
 
             std::string tau21Form = "(1.0+tau21_PtDependence"+yr+"*"+ (p == purCats[PURE_HP] ? "log("+MOD_MS+"/1000)" : "((0.054/0.041)*(-log("+MOD_MS+"/1000)))")+")";
             std::string brealForm = "(1.0+btag_eff"+yr+"*";
             if(b == btagCats[BTAG_L]) brealForm+= "(0.22-4.7*10^(-4)*"+MOD_MS+"+9.4*10^(-8)*"+MOD_MS+"^(2)))";
-            if(b == btagCats[BTAG_M]) brealForm+="(-0.27+3.3*10^(-4)*"+MOD_MS+"-3.7*10^(-8)*"+MOD_MS+"^(2)))";
+//            if(b == btagCats[BTAG_M]) brealForm+="(-0.27+3.3*10^(-4)*"+MOD_MS+"-3.7*10^(-8)*"+MOD_MS+"^(2)))";
             if(b == btagCats[BTAG_T]) brealForm+="(0.22+3.8*10^(-4)*"+MOD_MS+"-4.9*10^(-8)*"+MOD_MS+"^(2)))";
             std::string jetForm = "(1.0+unclust"+yr+")*(1.0+jer"+yr+")*(1.0+0.5*jes"+yr+")";
             std::string uncForm = tau21Form+"*"+brealForm+"*"+jetForm;
-            double tau21Corr = p == purCats[PURE_HP]  ? 1.03 : 0.95;
+
+            double tau21Corr = (p == purCats[PURE_HP]  ? sf_tau21_HP_Run2 : sf_tau21_LP_Run2);
 
             card.addParametricYieldWithUncertainty(signalName,0,signalInputName(signalName,"yield.json"),
-                    1.0,"1.0",{},MOD_MS);
+            		tau21Corr,"1.0",{},MOD_MS);
 
 //            card.addParametricYieldWithUncertainty(signalName,0,signalInputName(signalName,"yield.json"),
 //                    tau21Corr,uncForm,{"tau21_PtDependence","btag_eff","unclust","jer","jes"},MOD_MS);
@@ -229,7 +237,7 @@ printf("dbg0\n");
             PDFAdder::InterpSysts mtKDESysts;
             mtKDESysts.addSyst("PT",{{systName("top","scale"),"1"},{systName("top","mt_rel_scale",b),"1"}});
             mtKDESysts.addSyst("OPT",{{systName("top","res"  ),"1"  }});
-            card.add1DBKGParametricShape(bkgSels[BKG_MT],MOD_MJ,inputName(bkgSels[BKG_MT],"MJJ_SFFit.json"),{{"hbb_scale"+yr,1}},{{"hbb_res"+yr,1}},MOD_MR,MOD_MJ);
+            card.add1DBKGParametricShape(bkgSels[BKG_MT],MOD_MJ,inputName(bkgSels[BKG_MT],"MJJ_SFFit.json"),{{"hbb_scale_top"+yr,1}},{{"hbb_res_top"+yr,1}},MOD_MR,MOD_MJ);
             card.addHistoShapeFromFile(bkgSels[BKG_MT],{MOD_MR}, inputName(bkgSels[BKG_MT],"MVV_template.root"),"histo",mtKDESysts,false,0,MOD_MR,true);
             card.conditionalProduct(bkgSels[BKG_MT],bkgSels[BKG_MT] + "_"+MOD_MJ,MOD_MJ,bkgSels[BKG_MT]+ "_"+MOD_MR);
 
@@ -300,8 +308,9 @@ printf("dbg0\n");
 //            card.addParamSystematic("jes",0.0,0.01);
 //            card.addParamSystematic("jer",0.0,0.01);
 
-            card.addParamSystematic("hbb_scale"+yr,0.0,0.0094);
-            card.addParamSystematic("hbb_res"+yr,0.0,0.2);
+            // soft-drop mass scale and resolution
+            addSDMassSysts(card,yr);
+
             //KDE shape systematics
             card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"PTX",b) ,0.0,0.5);
             card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"OPTX",b),0.0,1.0);
@@ -333,13 +342,13 @@ printf("dbg0\n");
             //Signal
             //---------------------------------------------------------------------------------------------------
             card.add2DSignalParametricShape(signalName,MOD_MJ,MOD_MR, signalInputName(signalName,"2D_fit.json"),
-                                 StrFlts(),StrFlts(),StrFlts(),StrFlts(), b == btagCats[BTAG_L],MOD_MS);
+            		{{"hbb_scale"+yr,1}},{{"hbb_res"+yr,1}},StrFlts(),StrFlts(), b == btagCats[BTAG_L],MOD_MS);
 //            card.add2DSignalParametricShape(signalName,MOD_MJ,MOD_MR, signalInputName(signalName,"2D_fit.json"),
 //                                 {{"hbb_scale",1}},{{"hbb_res",1}},{{"unclust",0.5},{"jes",1},{"jer",0.5}},{{"unclust",0.5},{"jes",2},{"jer",5}}, b == btagCats[BTAG_L],MOD_MS);
 
             std::string brealForm = "(1.0+btag_eff*";
             if(b == btagCats[BTAG_L]) brealForm+= "(0.22-4.7*10^(-4)*"+MOD_MS+"+9.4*10^(-8)*"+MOD_MS+"^(2)))";
-            if(b == btagCats[BTAG_M]) brealForm+="(-0.27+3.3*10^(-4)*"+MOD_MS+"-3.7*10^(-8)*"+MOD_MS+"^(2)))";
+//            if(b == btagCats[BTAG_M]) brealForm+="(-0.27+3.3*10^(-4)*"+MOD_MS+"-3.7*10^(-8)*"+MOD_MS+"^(2)))";
             if(b == btagCats[BTAG_T]) brealForm+="(0.22+3.8*10^(-4)*"+MOD_MS+"-4.9*10^(-8)*"+MOD_MS+"^(2)))";
             std::string jetForm = "(1.0+unclust)*(1.0+jer)*(1.0+0.5*jes)";
             std::string uncForm = brealForm+"*"+jetForm;
@@ -390,7 +399,7 @@ printf("dbg0\n");
             PDFAdder::InterpSysts mtKDESysts;
             mtKDESysts.addSyst("PT",{{systName("top","scale"),"1"},{systName("top","mt_rel_scale",b),"1"}});
             mtKDESysts.addSyst("OPT",{{systName("top","res"  ),"1"  }});
-            card.add1DBKGParametricShape(bkgSels[BKG_MT],MOD_MJ,inputName(bkgSels[BKG_MT],"MJJ_SFFit.json"),{{"hbb_scale"+yr,1}},{{"hbb_res"+yr,1}},MOD_MR,MOD_MJ);
+            card.add1DBKGParametricShape(bkgSels[BKG_MT],MOD_MJ,inputName(bkgSels[BKG_MT],"MJJ_SFFit.json"),{{"hbb_scale_top"+yr,1}},{{"hbb_res_top"+yr,1}},MOD_MR,MOD_MJ);
             card.addHistoShapeFromFile(bkgSels[BKG_MT],{MOD_MR}, inputName(bkgSels[BKG_MT],"MVV_template.root"),"histo",mtKDESysts,false,0,MOD_MR,true);
             card.conditionalProduct(bkgSels[BKG_MT],bkgSels[BKG_MT] + "_"+MOD_MJ,MOD_MJ,bkgSels[BKG_MT]+ "_"+MOD_MR);
             printf("dbg5\n");
