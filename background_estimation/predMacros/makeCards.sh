7fd22d4c5e87b9b368e9c6d353ec838c8bd8212f
Brent Stone <brentstone01@gmail.com>
2020-10-05 16:15:27 -0700

    shorter command to run card, now making cards on LPC within CMSSW env for running limits, so no more copying needed

--- background_estimation/predMacros/makeCards.sh
+++ background_estimation/predMacros/makeCards.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# . /Users/nmccoll/Dropbox/Work/GitRepositories/TreeAnalyzer/background_estimation/predMacros/makeCards.sh baseline 0 0 2 /Users/nmccoll/Dropbox/Work/GitRepositories/TreeAnalyzer/background_estimation/predMacros/
-# . /Users/brentstone/Dropbox/Physics/GitRepos/TreeAnalyzer/background_estimation/predMacros/makeCards.sh baseline 0 0 2 /Users/brentstone/Dropbox/Physics/GitRepos/TreeAnalyzer/background_estimation/predMacros/
+# . /Users/brentstone/Dropbox/Physics/GitRepos/TreeAnalyzer/background_estimation/predMacros/makeCards.sh baseline 0 0 0
 
 catName=$1
 runSR=$2
 signal=$3
 channel=$4      # 0 for 1l + 2l, 1 for 1l, 2 for 2l
-macroLoc=$5
 
+macroLoc="/Users/brentstone/Dropbox/Physics/GitRepos/TreeAnalyzer/background_estimation/predMacros/"
 
 dirName="${catName}"
 if [ "${runSR}" = "1" ] ; then
@@ -32,10 +31,10 @@ eval $RCMD
 
 . comp.sh
 cd ..
-RCMD="scp -r ${dirName} bwstone@cmslpc26.fnal.gov:/uscms/home/bwstone/nobackup/TreeAnalyzer/combine/"
-if [ "${runSR}" = "0" ]; then
-    eval $RCMD
-fi
+#RCMD="scp -r ${dirName} bwstone@cmslpc26.fnal.gov:/uscms/home/bwstone/nobackup/TreeAnalyzer/combine/"
+#if [ "${runSR}" = "0" ]; then
+#    eval $RCMD
+#fi
 cd ${dirName}
 RCMD="text2workspace.py combinedCard.txt -o combined.root"
 eval $RCMD
