b11f26eaed68167db7dfa19ce701c111d9cc614d
Brent Stone <brentstone01@gmail.com>
2020-11-02 14:15:01 -0800

    updated systematics, playing with rebinning

--- background_estimation/predMacros/makeCard.C
+++ background_estimation/predMacros/makeCard.C
@@ -5,6 +5,11 @@ using namespace CutConstants;
 using namespace SystConstants;
 using namespace ASTypes;
 
+std::vector<double> hhMassRebins = {
+        700,750,800,850,900,950,1000,1050,1100,1150,1200,1250,1300,1350,1400,1450,1500,1550,1600,1650,
+        1700,1750,1800,1850,1900,1950,2000,2100,2200,2300,2400,2500,2600,2700,2800,2900,3000,3100,
+        3250,3400,3550,3700,3850,4000
+};
 
 std::string getSystName (const std::string& prefix, const std::string& proc,const std::string& name, const std::string& sel){
     std::string sn = prefix;
@@ -36,6 +41,10 @@ void addSDMassSysts(DataCardMaker& card, std::string yr) {
     card.addParamSystematic("hbb_scale_top"+yr,sf_sdmass_scale_Run2-1.0,unc_sdmass_scale_Run2);
     card.addParamSystematic("hbb_res"+yr,    sf_sdmass_res_Run2-1.0,unc_sdmass_res_Run2);
     card.addParamSystematic("hbb_res_top"+yr,sf_sdmass_res_Run2-1.0,unc_sdmass_res_Run2);
+//    card.addParamSystematic("hbb_scale"+yr,    sf_sdmass_scale_Run2-1.0,0.03);
+//    card.addParamSystematic("hbb_scale_top"+yr,sf_sdmass_scale_Run2-1.0,0.03);
+//    card.addParamSystematic("hbb_res"+yr,    sf_sdmass_res_Run2-1.0,0.45);
+//    card.addParamSystematic("hbb_res_top"+yr,sf_sdmass_res_Run2-1.0,0.45);
 };
 
 void go(const int insig, const std::string& filename, const std::string& mainDir, REGION reg, int channel){
@@ -91,6 +100,11 @@ void go(const int insig, const std::string& filename, const std::string& mainDir
 
                 makeSearchVars(card);
 
+                // test
+//                card.rebinY(hhMassRebins);
+                card.rebinX(22,30,162);
+//                card.rebinX(28,30,198);
+
 //            auto addSignalSystematics = [&]() {
 //            	card.addSystematic("yield","lnN",{{signalName,systematics.yield}});
 //            };
@@ -144,31 +158,32 @@ void go(const int insig, const std::string& filename, const std::string& mainDir
             addSDMassSysts(card,yr);
 
             // QG bkg KDE shape systematics (X = mbb, Y = mhh -- morphing: PT = m, OPT = 1/m)
-            card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"PTX",b) ,0.0,0.5);
-            card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"OPTX",b),0.0,1.0);
-            card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"PTY")   ,0.0,1.0);
-            card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"OPTY")  ,0.0,1.0);
+            card.addParamSystematic(systName(bkgSels[BKG_QG] ,"PTX",b+"_1l") ,0.0,0.5);
+            card.addParamSystematic(systName(bkgSels[BKG_QG] ,"OPTX",b+"_1l"),0.0,0.5);
+            card.addParamSystematic(systName(bkgSels[BKG_QG] ,"PTY")         ,0.0,1.0);
+            card.addParamSystematic(systName(bkgSels[BKG_QG] ,"OPTY")        ,0.0,1.0);
 
             // lost tW mbb resolution (OPT = 1/m) and scale (PT = m)
-            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"PTX" ,b),0.0,0.3);
-            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"OPTX",b),0.0,0.6);
+            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"PTX" ,b+"_1l"),0.0,0.4);
+            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"OPTX",b+"_1l"),0.0,0.6);
 
             // Normalization (correlate top-derived bkgs, separate QG)
             card.addSystematic(systName(bkgSels[BKG_QG],"norm")  ,"lnN",{{bkgSels[BKG_QG],1.5}});
-            card.addSystematic(systName("top","norm")            ,"lnN",{{bkgSels[BKG_LOSTTW],1.25},{bkgSels[BKG_MW],1.25},{bkgSels[BKG_MT],1.25}});
+            card.addSystematic(systName("top","norm")            ,"lnN",{{bkgSels[BKG_LOSTTW],1.30},{bkgSels[BKG_MW],1.30},{bkgSels[BKG_MT],1.30}});
 
             //top HH resolution and scale (correlate all top-derived bkgs: lost tW, mW, mT)
-            card.addParamSystematic(systName("top","res"  ) ,0.0,0.20);
-            card.addParamSystematic(systName("top","scale") ,0.0,0.25);
-            card.addParamSystematic(systName("top","mt_rel_scale",b) ,0.0,0.25);
-            card.addParamSystematic(systName("top","lostmw_rel_scale",b) ,0.0,0.25);
+            card.addParamSystematic(systName("top","res"  ) ,0.0,0.30);
+            card.addParamSystematic(systName("top","scale") ,0.0,0.30);
+            card.addParamSystematic(systName("top","mt_rel_scale",b+"_1l") ,0.0,0.30);
+            card.addParamSystematic(systName("top","lostmw_rel_scale",b+"_1l") ,0.0,0.30);
 
+            // avoid negative numbers for lnN systs in NonTopCR
             if(reg == REG_NONTOPCR){
-                card.addSystematic(systName("top","tFrac",b)         ,"lnN",{{bkgSels[BKG_MT],1.0+0.25},{bkgSels[BKG_MW],1.0-0.25*rate_mt/rate_mw}});
-                card.addSystematic(systName("top","lostFrac",b)      ,"lnN",{{bkgSels[BKG_LOSTTW],1.0+0.25},{bkgSels[BKG_MW],1.0-0.25*rate_lostTW/rate_mw}});
+                card.addSystematic(systName("top","tFrac",b+"_1l")         ,"lnN",{{bkgSels[BKG_MT],1.0+0.30},{bkgSels[BKG_MW],1.0-0.30*rate_mt/rate_mw}});
+                card.addSystematic(systName("top","lostFrac",b+"_1l")      ,"lnN",{{bkgSels[BKG_LOSTTW],1.0+0.30},{bkgSels[BKG_MW],1.0-0.30*rate_lostTW/rate_mw}});
             } else {
-                card.addSystematic(systName("top","wFrac",b)         ,"lnN",{{bkgSels[BKG_MW],1.0+0.25},{bkgSels[BKG_MT],1.0-0.25*rate_mw/rate_mt}});
-                card.addSystematic(systName("top","lostFrac",b)      ,"lnN",{{bkgSels[BKG_LOSTTW],1.0+0.25},{bkgSels[BKG_MT],1.0-0.25*rate_lostTW/rate_mt}});
+                card.addSystematic(systName("top","wFrac",b+"_1l")         ,"lnN",{{bkgSels[BKG_MW],1.0+0.30},{bkgSels[BKG_MT],1.0-0.30*rate_mw/rate_mt}});
+                card.addSystematic(systName("top","lostFrac",b+"_1l")      ,"lnN",{{bkgSels[BKG_LOSTTW],1.0+0.30},{bkgSels[BKG_MT],1.0-0.30*rate_lostTW/rate_mt}});
             }
 printf("dbg0\n");
             //---------------------------------------------------------------------------------------------------
@@ -201,8 +216,8 @@ printf("dbg0\n");
             //QG
             //---------------------------------------------------------------------------------------------------
             PDFAdder::InterpSysts qgKDESysts;
-            qgKDESysts.addSyst("PTX",{{systName(bkgSels[BKG_QG],"PTX",b),"1"  }});
-            qgKDESysts.addSyst("OPTX",{{systName(bkgSels[BKG_QG],"OPTX",b),"1"  }});
+            qgKDESysts.addSyst("PTX",{{systName(bkgSels[BKG_QG],"PTX",b+"_1l"),"1"  }});
+            qgKDESysts.addSyst("OPTX",{{systName(bkgSels[BKG_QG],"OPTX",b+"_1l"),"1"  }});
             qgKDESysts.addSyst("PTY",{{systName(bkgSels[BKG_QG],"PTY"),"1"  }});
             qgKDESysts.addSyst("OPTY",{{systName(bkgSels[BKG_QG],"OPTY"),"1"  }});
     //        qgKDESysts.addSyst("PT2Y",{{systName(bkgSels[BKG_QG],"PT2Y"),"1"  }});
@@ -213,9 +228,9 @@ printf("dbg0\n");
             //Lost t/W
             //---------------------------------------------------------------------------------------------------
             PDFAdder::InterpSysts twKDESysts;
-            twKDESysts.addSyst("PTX",{{systName(bkgSels[BKG_LOSTTW],"PTX",b),"1"  }});
-            twKDESysts.addSyst("OPTX",{{systName(bkgSels[BKG_LOSTTW],"OPTX",b),"1"  }});
-            twKDESysts.addSyst("PTY",{{systName("top","scale"),"1"},{systName("top","lostmw_rel_scale",b),"1"}});
+            twKDESysts.addSyst("PTX",{{systName(bkgSels[BKG_LOSTTW],"PTX",b+"_1l"),"1"  }});
+            twKDESysts.addSyst("OPTX",{{systName(bkgSels[BKG_LOSTTW],"OPTX",b+"_1l"),"1"  }});
+            twKDESysts.addSyst("PTY",{{systName("top","scale"),"1"},{systName("top","lostmw_rel_scale",b+"_1l"),"1"}});
             twKDESysts.addSyst("OPTY",{{systName("top","res"  ),"1"  }});
             card.addHistoShapeFromFile(bkgSels[BKG_LOSTTW],{MOD_MJ,MOD_MR},inputName(bkgSels[BKG_LOSTTW],"2D_template.root"),"histo",twKDESysts);
             printf("dbg3\n");
@@ -224,7 +239,7 @@ printf("dbg0\n");
             //mW
             //---------------------------------------------------------------------------------------------------
             PDFAdder::InterpSysts mwKDESysts;
-            mwKDESysts.addSyst("PT",{{systName("top","scale"),"1"},{systName("top","lostmw_rel_scale",b),"1"}});
+            mwKDESysts.addSyst("PT",{{systName("top","scale"),"1"},{systName("top","lostmw_rel_scale",b+"_1l"),"1"}});
             mwKDESysts.addSyst("OPT",{{systName("top","res"  ),"1"  }});
             card.add1DBKGParametricShape(bkgSels[BKG_MW],MOD_MJ,inputName(bkgSels[BKG_MW],"MJJ_SFFit.json"),{{"hbb_scale"+yr,1}},{{"hbb_res"+yr,1}},MOD_MR,MOD_MJ);
             card.addHistoShapeFromFile(bkgSels[BKG_MW],{MOD_MR}, inputName(bkgSels[BKG_MW],"MVV_template.root"),"histo",mwKDESysts,false,0,MOD_MR,true);
@@ -235,7 +250,7 @@ printf("dbg0\n");
             //mt
             //---------------------------------------------------------------------------------------------------
             PDFAdder::InterpSysts mtKDESysts;
-            mtKDESysts.addSyst("PT",{{systName("top","scale"),"1"},{systName("top","mt_rel_scale",b),"1"}});
+            mtKDESysts.addSyst("PT",{{systName("top","scale"),"1"},{systName("top","mt_rel_scale",b+"_1l"),"1"}});
             mtKDESysts.addSyst("OPT",{{systName("top","res"  ),"1"  }});
             card.add1DBKGParametricShape(bkgSels[BKG_MT],MOD_MJ,inputName(bkgSels[BKG_MT],"MJJ_SFFit.json"),{{"hbb_scale_top"+yr,1}},{{"hbb_res_top"+yr,1}},MOD_MR,MOD_MJ);
             card.addHistoShapeFromFile(bkgSels[BKG_MT],{MOD_MR}, inputName(bkgSels[BKG_MT],"MVV_template.root"),"histo",mtKDESysts,false,0,MOD_MR,true);
@@ -272,9 +287,12 @@ printf("dbg0\n");
             };
 
             //Make search variables
-            card.addVar(MOD_MJ,100,0,1000,false);
-            card.addVar(MOD_MR,1000,0,10000,false);
-            card.addVar(MOD_MS,2000,true);
+            makeSearchVars(card);
+
+            // test
+//            card.rebinY(hhMassRebins);
+            card.rebinX(22,30,162);
+//            card.rebinX(28,30,198);
 
             //---------------------------------------------------------------------------------------------------
             //Get rates and contributions for backgrounds
@@ -308,33 +326,36 @@ printf("dbg0\n");
 //            card.addParamSystematic("jes",0.0,0.01);
 //            card.addParamSystematic("jer",0.0,0.01);
 
-            // soft-drop mass scale and resolution
+            // Soft-drop mass scale and resolution
             addSDMassSysts(card,yr);
 
-            //KDE shape systematics
-            card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"PTX",b) ,0.0,0.5);
-            card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"OPTX",b),0.0,1.0);
-            card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"PTY")   ,0.0,1.0);
-            card.addParamSystematic(systName(bkgSels[BKG_QG]    ,"OPTY")  ,0.0,1.0);
-
-            //top HH resolution and scale
-            card.addParamSystematic(systName("top","res"  ) ,0.0,0.20);
-            card.addParamSystematic(systName("top","scale") ,0.0,0.25);
-            card.addParamSystematic(systName("top","mt_rel_scale",b) ,0.0,0.25);
-            card.addParamSystematic(systName("top","lostmw_rel_scale",b) ,0.0,0.25);
-            //top Hbb resolution and scale
-            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"PTX" ,b),0.0,0.3);
-            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"OPTX",b),0.0,0.6);
-            //Normalization
+            // QG normalization and KDE shape systematics (PT = scale (m), OPT = res (1/m))
             card.addSystematic(systName(bkgSels[BKG_QG],"norm")  ,"lnN",{{bkgSels[BKG_QG],1.5}});
-            card.addSystematic(systName("top","norm")            ,"lnN",{{bkgSels[BKG_LOSTTW],1.25},{bkgSels[BKG_MW],1.25},{bkgSels[BKG_MT],1.25}});
+            card.addParamSystematic(systName(bkgSels[BKG_QG],"PTX",b+"_2l") ,0.0,0.5);
+            card.addParamSystematic(systName(bkgSels[BKG_QG],"OPTX",b+"_2l"),0.0,0.5);
+            card.addParamSystematic(systName(bkgSels[BKG_QG],"PTY")         ,0.0,1.0);
+            card.addParamSystematic(systName(bkgSels[BKG_QG],"OPTY")        ,0.0,1.0);
+
+            // Lost tW mbb KDE systematics (PTX = scale (m), OPTX = res (1/m))
+            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"PTX" ,b+"_2l"),0.0,0.5);
+            card.addParamSystematic(systName(bkgSels[BKG_LOSTTW],"OPTX",b+"_2l"),0.0,0.6);
+
+            // Top normalization and HH resolution (1/m) and scale (m)
+            card.addSystematic(systName("top","norm"),"lnN",{{bkgSels[BKG_LOSTTW],1.30},
+            		{bkgSels[BKG_MW],1.30},{bkgSels[BKG_MT],1.30}});
+            card.addParamSystematic(systName("top","res"  ) ,0.0,0.30);
+            card.addParamSystematic(systName("top","scale") ,0.0,0.40);
+            card.addParamSystematic(systName("top","mt_rel_scale",b+"_2l") ,0.0,0.40);
+            card.addParamSystematic(systName("top","lostmw_rel_scale",b+"_2l") ,0.0,0.40);
+
+            //Normalization
 
             if(reg == REG_NONTOPCR){
-                card.addSystematic(systName("top","tFrac",b)         ,"lnN",{{bkgSels[BKG_MT],1.0+0.25},{bkgSels[BKG_MW],1.0-0.25*rate_mt/rate_mw}});
-                card.addSystematic(systName("top","lostFrac",b)      ,"lnN",{{bkgSels[BKG_LOSTTW],1.0+0.25},{bkgSels[BKG_MW],1.0-0.25*rate_lostTW/rate_mw}});
+                card.addSystematic(systName("top","tFrac",b+"_2l")    ,"lnN",{{bkgSels[BKG_MT],1.0+0.30},{bkgSels[BKG_MW],1.0-0.30*rate_mt/rate_mw}});
+                card.addSystematic(systName("top","lostFrac",b+"_2l") ,"lnN",{{bkgSels[BKG_LOSTTW],1.0+0.30},{bkgSels[BKG_MW],1.0-0.30*rate_lostTW/rate_mw}});
             } else {
-                card.addSystematic(systName("top","wFrac",b)         ,"lnN",{{bkgSels[BKG_MW],1.0+0.25},{bkgSels[BKG_MT],1.0-0.25*rate_mw/rate_mt}});
-                card.addSystematic(systName("top","lostFrac",b)      ,"lnN",{{bkgSels[BKG_LOSTTW],1.0+0.25},{bkgSels[BKG_MT],1.0-0.25*rate_mt/rate_lostTW}}); // changed to mt / tw
+                card.addSystematic(systName("top","wFrac",b+"_2l")    ,"lnN",{{bkgSels[BKG_MW],1.0+0.30},{bkgSels[BKG_MT],1.0-0.30*rate_mw/rate_mt}});
+                card.addSystematic(systName("top","lostFrac",b+"_2l") ,"lnN",{{bkgSels[BKG_LOSTTW],1.0+0.30},{bkgSels[BKG_MT],1.0-0.30*rate_mt/rate_lostTW}}); // changed to mt / tw
             }
 
             //---------------------------------------------------------------------------------------------------
@@ -362,8 +383,8 @@ printf("dbg0\n");
             printf("dbg1\n");
 
             PDFAdder::InterpSysts qgKDESysts;
-            qgKDESysts.addSyst("PTX",{{systName(bkgSels[BKG_QG],"PTX",b),"1"  }});
-            qgKDESysts.addSyst("OPTX",{{systName(bkgSels[BKG_QG],"OPTX",b),"1"  }});
+            qgKDESysts.addSyst("PTX",{{systName(bkgSels[BKG_QG],"PTX",b+"_2l"),"1"  }});
+            qgKDESysts.addSyst("OPTX",{{systName(bkgSels[BKG_QG],"OPTX",b+"_2l"),"1"  }});
             qgKDESysts.addSyst("PTY",{{systName(bkgSels[BKG_QG],"PTY"),"1"  }});
             qgKDESysts.addSyst("OPTY",{{systName(bkgSels[BKG_QG],"OPTY"),"1"  }});
     //        qgKDESysts.addSyst("PT2Y",{{systName(bkgSels[BKG_QG],"PT2Y"),"1"  }});
@@ -374,9 +395,9 @@ printf("dbg0\n");
             //Lost t/W
             //---------------------------------------------------------------------------------------------------
             PDFAdder::InterpSysts twKDESysts;
-            twKDESysts.addSyst("PTX",{{systName(bkgSels[BKG_LOSTTW],"PTX",b),"1"  }});
-            twKDESysts.addSyst("OPTX",{{systName(bkgSels[BKG_LOSTTW],"OPTX",b),"1"  }});
-            twKDESysts.addSyst("PTY",{{systName("top","scale"),"1"},{systName("top","lostmw_rel_scale",b),"1"}});
+            twKDESysts.addSyst("PTX",{{systName(bkgSels[BKG_LOSTTW],"PTX",b+"_2l"),"1"  }});
+            twKDESysts.addSyst("OPTX",{{systName(bkgSels[BKG_LOSTTW],"OPTX",b+"_2l"),"1"  }});
+            twKDESysts.addSyst("PTY",{{systName("top","scale"),"1"},{systName("top","lostmw_rel_scale",b+"_2l"),"1"}});
             twKDESysts.addSyst("OPTY",{{systName("top","res"  ),"1"  }});
             card.addHistoShapeFromFile(bkgSels[BKG_LOSTTW],{MOD_MJ,MOD_MR},inputName(bkgSels[BKG_LOSTTW],"2D_template.root"),"histo",twKDESysts);
             printf("dbg3\n");
@@ -385,7 +406,7 @@ printf("dbg0\n");
             //mW
             //---------------------------------------------------------------------------------------------------
             PDFAdder::InterpSysts mwKDESysts;
-            mwKDESysts.addSyst("PT",{{systName("top","scale"),"1"},{systName("top","lostmw_rel_scale",b),"1"}});
+            mwKDESysts.addSyst("PT",{{systName("top","scale"),"1"},{systName("top","lostmw_rel_scale",b+"_2l"),"1"}});
             mwKDESysts.addSyst("OPT",{{systName("top","res"  ),"1"  }});
             card.add1DBKGParametricShape(bkgSels[BKG_MW],MOD_MJ,inputName(bkgSels[BKG_MW],"MJJ_SFFit.json"),{{"hbb_scale"+yr,1}},{{"hbb_res"+yr,1}},MOD_MR,MOD_MJ);
             card.addHistoShapeFromFile(bkgSels[BKG_MW],{MOD_MR}, inputName(bkgSels[BKG_MW],"MVV_template.root"),"histo",mwKDESysts,false,0,MOD_MR,true);
@@ -396,7 +417,7 @@ printf("dbg0\n");
             //mt
             //---------------------------------------------------------------------------------------------------
             PDFAdder::InterpSysts mtKDESysts;
-            mtKDESysts.addSyst("PT",{{systName("top","scale"),"1"},{systName("top","mt_rel_scale",b),"1"}});
+            mtKDESysts.addSyst("PT",{{systName("top","scale"),"1"},{systName("top","mt_rel_scale",b+"_2l"),"1"}});
             mtKDESysts.addSyst("OPT",{{systName("top","res"  ),"1"  }});
             card.add1DBKGParametricShape(bkgSels[BKG_MT],MOD_MJ,inputName(bkgSels[BKG_MT],"MJJ_SFFit.json"),{{"hbb_scale_top"+yr,1}},{{"hbb_res_top"+yr,1}},MOD_MR,MOD_MJ);
             card.addHistoShapeFromFile(bkgSels[BKG_MT],{MOD_MR}, inputName(bkgSels[BKG_MT],"MVV_template.root"),"histo",mtKDESysts,false,0,MOD_MR,true);
