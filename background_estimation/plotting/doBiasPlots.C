bdbee7dbd5aaf00fb5ffe3f8749503846635116f
Brent Stone <brentstone01@gmail.com>
2020-12-03 14:08:14 -0800

    drawing and plot generation for bias tests

---
+++ background_estimation/plotting/doBiasPlots.C
@@ -0,0 +1,42 @@
+#include "TGraph.h"
+#include "TFile.h"
+#include "TGraphErrors.h"
+#include "HistoPlotting/include/Plotter.h"
+
+std::vector<double> masses = {800,900,1000,1200,1500,1800,2000,2500,3000,3500};
+std::map<int,TString> binIds = { {1,"mu"}, {2,"mu_unc"}, {3,"dMean"}, {4,"dMedian"}, {5,"dStdDev"} };
+std::vector<TString> rexcls = {"r1","r2","r5"};
+std::map<TString,TString> rStrs = { {"r1","r_{inj} = r_{excl}"}, {"r2","r_{inj} = 2r_{excl}"}, {"r5","r_{inj} = 5r_{excl}"} };
+
+void doBiasPlots(TString dir) {
+
+	TFile *f = TFile::Open(dir+"/plots/biasTest.root");
+	for(const auto& id : binIds) {
+		if(id.first == 2) continue;
+
+		Plotter *p = new Plotter();
+
+		for(const auto& r : rexcls) {
+			TGraphErrors *g = new TGraphErrors();
+
+			for(unsigned im=0; im<masses.size(); ++im) {
+				const auto& m = masses[im];
+				TH1 *h = (TH1*)f->Get(TString::Format("biasDist_postfit_%s_%d_quants",r.Data(),int(m)));
+				if(!h) continue;
+
+				double val = h->GetBinContent(id.first);
+				g->SetPoint(im,m,val);
+				if(id.first == 1) g->SetPointError(im,0,h->GetBinContent(id.first+1));
+
+//				std::cout<<"m"<<m<<" - "<<r.Data()<<std::endl<<id.second<<" = "<<val<<std::endl<<std::endl;
+
+			}
+
+			p->addGraph(g,rStrs[r],-1,1,2,20,1,true,true,false,"L");
+		}
+		p->setMinMax(-2,2);
+		p->setXTitle("M_{X} [GeV]");
+		p->draw(false,id.second);
+	}
+
+}
---
+++ background_estimation/plotting/plotBiasTests.C
@@ -0,0 +1,217 @@
+#include <vector>
+#include "TFile.h"
+#include "TH1.h"
+#include "TTree.h"
+#include "TStyle.h"
+#include "TGraph.h"
+#include "TArrow.h"
+#include "TFitResultPtr.h"
+#include "TFitResult.h"
+#include "HistoPlotting/include/Plotter.h"
+#include "HistoPlotting/include/PlotTools.h"
+#include "plotTestHelper.h"
+
+std::vector<TObject*> writeables;
+class Dummy {
+public:
+    Dummy(const std::string& outName = "") : outName(outName) {};
+    ~Dummy() {
+        if(outName.size()){
+            TFile * f = new TFile((outName+".root").c_str(),"recreate");
+            f->cd();
+            for(auto * w : writeables){
+                w->Write();
+                w->Print((outName +"_"+w->GetTitle() +".pdf").c_str());
+            }
+            f->Close();
+        }
+    }
+    std::string outName;
+};
+
+std::vector<std::pair<int,double>> massR1s = {
+		{800,0.0240234},
+		{900,0.0141113},
+		{1000,0.0093016},
+		{1200,0.00524902},
+		{1500,0.003064},
+		{1800,0.00213623},
+		{2000,0.00175171},
+		{2500,0.0012085},
+		{3000,0.000946},
+		{3500,0.000836182}
+};
+
+
+std::vector<std::pair<int,double>> massR2s = {
+		{800,0.0480468},
+		{900,0.0282226},
+		{1000,0.0186032},
+		{1200,0.01049804},
+		{1500,0.006128},
+		{1800,0.00427246},
+		{2000,0.00350342},
+		{2500,0.002417},
+		{3000,0.001892},
+		{3500,0.001672364}
+};
+
+std::vector<std::pair<int,double>> massR5s = {
+		{800,0.120117},
+		{900,0.0705565},
+		{1000,0.046508},
+		{1200,0.0262451},
+		{1500,0.01532},
+		{1800,0.01068115},
+		{2000,0.0087585500},
+		{2500,0.0060425},
+		{3000,0.00473},
+		{3500,0.00418091}
+};
+
+void doBiasTest(std::vector<TObject*>& writeables, const std::string& limitBaseName ){
+
+    auto makeBiasPlot = [&](const std::string& filename, const std::string& hName, const double rV, TFitResultPtr& fitres,
+    						double& dMean, double& dMedian, double& dStdDev) ->TH1*  {
+        TFile * f = new TFile(filename.c_str(),"read");
+        if(!f) return 0;
+        TTree * tree = 0;
+        f->GetObject("tree_fit_sb",tree);
+        if(!tree){return 0;}
+        double fr=0;
+        double frerr=0;
+        int numbadnll=0;
+        tree->SetBranchAddress("r",&fr);
+        tree->SetBranchAddress("rErr",&frerr);
+        tree->SetBranchAddress("numbadnll",&numbadnll);
+
+        std::vector<double> biases;
+        for(unsigned int iE = 0; tree->GetEntry(iE); ++iE ){
+            if(numbadnll!=0) continue;
+        	if(frerr == 0) continue;
+            if(std::fabs((fr - rV)/frerr ) > 5) continue;
+            biases.push_back( (fr - rV)/frerr   );
+        }
+        f->Close();
+
+        std::sort(biases.begin(),biases.end(), [](const double a, const double b){return a < b;});
+        printf("biases size = %d",int(biases.size()));
+        dMedian = biases[int(0.5*biases.size())];
+        dMean = 0;
+        dStdDev = 0;
+        for(const auto& b : biases) dMean += b/biases.size();
+        for(const auto& b : biases) dStdDev += (b-dMean)*(b-dMean)/biases.size();
+        dStdDev = std::sqrt(dStdDev);
+
+//        std::cout<<filename<<std::endl;
+//        printf("distMean = %f\ndistMedian = %f\ndistStdDev = %f\n\n",dMean,dMedian,dStdDev);
+
+        TH1 * h = new TH1D(hName.c_str(),";signal strength pull",10, -5,5);
+        h->SetDirectory(0);
+        TH1::AddDirectory(kFALSE);
+        for(auto b : biases) h->Fill(b);
+        auto c = new TCanvas(hName.c_str(),hName.c_str());
+        h->Draw();
+        fitres = h->Fit("gaus","S");;
+        writeables.push_back(c);
+
+        TH1D *hStats = new TH1D((hName+"_quants").c_str(),"",5,0,5);
+        hStats->SetBinContent(1,fitres->GetParams()[1]);
+        hStats->SetBinContent(2,fitres->GetErrors()[1]);
+        hStats->SetBinContent(3,dMean);
+        hStats->SetBinContent(4,dMedian);
+        hStats->SetBinContent(5,dStdDev);
+        writeables.push_back(hStats);
+
+        //        std::sort(biases.begin(),biases.end(), [](const double a, const double b){return a < b;});
+        //        double nToys = biases.size();
+        //        h->SetDirectory(0);
+        //        TH1::AddDirectory(kFALSE);
+        //        for(auto b : biases) h->Fill(b);
+        //        std::cout << hName <<" "<< biases[nToys*0.5]<<std::endl;
+        //        return h;
+
+        return h;
+
+    };
+
+    Plotter * p = new Plotter();
+    Plotter * pw = new Plotter();
+
+    std::vector<std::vector<double>> means(massR1s.size());
+    std::vector<std::vector<double>> meanErs(massR1s.size());
+
+    std::vector<std::vector<double>> distMeans(massR1s.size());
+    std::vector<std::vector<double>> distMedians(massR1s.size());
+    std::vector<std::vector<double>> distStdDevs(massR1s.size());
+
+    auto doSet = [&] (const std::string& label,const std::string& plotlabel, const std::vector<std::pair<int,double>>& massRs ){
+        TGraphErrors * gr = new TGraphErrors();
+        TGraphErrors * grw = new TGraphErrors();
+        int iP = 0;
+
+        for(unsigned int iM = 0; iM < massRs.size(); ++iM){
+            const auto& mr = massRs[iM];
+            std::cout<<mr.first<<std::endl;
+            TFitResultPtr fitres ;
+            double dMean=0, dMed=0, dSD=0;
+            auto dist = makeBiasPlot(limitBaseName + "/biasInput_"+label+"_m"+int2Str(mr.first)+".root",
+                    "biasDist_"+label+"_"+int2Str(mr.first), mr.second,fitres,dMean,dMed,dSD);
+            if(dist){
+                auto pars = fitres->GetParams();
+                auto errs = fitres->GetErrors();
+                gr->SetPoint(iP,double(mr.first),pars[1]);
+                gr->SetPointError(iP,0,errs[1]);
+                grw->SetPoint(iP,double(mr.first),pars[2]);
+                grw->SetPointError(iP,0,errs[2]);
+                means[iM].push_back(pars[1]);
+                meanErs[iM].push_back(errs[1]);
+
+                distMeans[iM].push_back(dMean);
+                distMedians[iM].push_back(dMed);
+                distStdDevs[iM].push_back(dSD);
+            }
+            ++iP;
+        }
+        if(iP){
+            p->addGraph(gr,plotlabel);
+            pw->addGraph(grw,plotlabel);
+        }
+    };
+
+    doSet("postfit_r1","postfit b-model: r=excluded"  ,massR1s);
+    doSet("postfit_r2","postfit b-model: r=2*excluded",massR2s);
+    doSet("postfit_r5","postfit b-model: r=5*excluded",massR5s);
+
+
+    p->setYTitle("signal strength bias");
+    p->setXTitle((sigMCS.title).c_str());
+    auto c = p->draw(false,"signalInjectTest_bias");
+    c->SetTitle("signalInjectTest_bias");
+    writeables.push_back(c);
+
+    pw->setYTitle("signal strength width");
+    pw->setXTitle((sigMCS.title).c_str());
+    auto cw = pw->draw(false,"signalInjectTest_width");
+    cw->SetTitle("signalInjectTest_width");
+    writeables.push_back(cw);
+
+    std::cout << std::endl << std::endl;
+    for(unsigned int iM = 0; iM < massR1s.size(); ++iM){
+        std::cout << "mx = "<<int2Str(massR1s[iM].first) << std::endl;
+        for(unsigned int iR = 0; iR < means[iM].size(); ++iR ){
+            std::cout << TString::Format("rexcl %d\nfitMu = %f +- %f\n",iR+1,means[iM][iR],meanErs[iM][iR]);
+            std::cout << TString::Format("dMean = %f\ndMedian = %f\ndStdDev = %f\n\n",distMeans[iM][iR],distMedians[iM][iR],
+            		distStdDevs[iM][iR]);
+        }
+
+    }
+    std::cout << std::endl << std::endl;
+}
+
+void plotBiasTests(std::string limDir) {
+
+	std::string outName = limDir+"/plots/biasTest";
+	doBiasTest(writeables,limDir);
+	Dummy d(outName);
+}
